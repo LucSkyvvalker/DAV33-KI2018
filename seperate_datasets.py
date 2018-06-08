bb54109b00e2a674e03c03aea03cf6ed2b433004
Luc Vink <lucjanvink@gmail.com>
2018-06-08 12:37:01 +0200

    unit-lists.py finished, seperate_datasets.py fixed

--- seperate_datasets.py
+++ seperate_datasets.py
@@ -6,7 +6,13 @@ import pandas as pd
 
 
 # loop over the dataset and write all matches to knew file
-with open('dataset.csv') as fIn, open("Riceset.csv", "w") as fOut:
+counter = 0
+with open('dataset.csv', encoding="latin-1") as fIn, open("Riceset.csv", "w", encoding="latin-1") as fOut:
     for line in fIn:
         if 'Rice' in line:
             fOut.write(line)
+            counter += 1
+        # dont forget the column names
+        elif counter == 0:
+            fOut.write(line)
+            counter += 1
--- unit_lists.py
+++ unit_lists.py
@@ -1,5 +1,9 @@
 import math
 
+###
+# a massive function that checks the given measurement trough if-elif statements
+# and converts the weight accordingly
+##
 def convert_weights(weight, measurement)
     if measurement === 'KG':
         new_weight = weight
@@ -41,6 +45,50 @@ elif measurement == '1.5 KG':
         new_weight = weight*750
     elif measurement == 'Cuartilla':
         new_weight = weight*2.875575
-    elif measurement == 'Pound':
-        new_weight = weight*0.45359237
+    elif measurement == '125 G':
+        new_weight = weight*0.125
+    elif measurement == '11.5 KG':
+        new_weight = weight*11.5
+    elif measurement == '380 G':
+        new_weight = weight*0.380
+    elif measurement == '385 G':
+        new_weight = weight*0.385
+    elif measurement == '185 G':
+        new_weight = weight*0.185
+    elif measurement == '85 G':
+        new_weight = weight*0.085
+    elif measurement == '91 KG':
+        new_weight = weight*91
+    elif measurement == '650 G':
+        new_weight = weight*0.650
+    elif measurement == '115 G':
+        new_weight = weight*0.115
+    elif measurement == '350 G':
+        new_weight = weight*0.350
+    elif measurement == '1.8 KG':
+        new_weight = weight*1.8
+    elif measurement == '2 KG':
+        new_weight = weight*2
+    elif measurement == '300 G':
+        new_weight = weight*0.300
+    elif measurement == '160 G':
+        new_weight = weight*0.160
+    elif measurement == '5 KG':
+        new_weight = weight*5
+    elif measurement == '200 G':
+        new_weight = weight*0.2
+    elif measurement == '10 KG':
+        new_weight = weight*10
+    elif measurement == '750 G':
+        new_weight = weight*0.75
+    elif measurement == '12 KG':
+        new_weight = weight*12
+    elif measurement == '18 KG':
+        new_weight = weight*18
+    elif measurement == '60 KG':
+        new_weight = weight*60
+    elif measurement == '3 KG':
+        new_weight = weight*3
+    elif measurement == '3.5 KG':
+        new_weight = weight*3.5
     return(round(new_weight, 2))
