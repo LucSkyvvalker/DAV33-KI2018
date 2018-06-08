62fd0f4671e68fd6000ad3b1b8efd6523c955793
Luc Vink <lucjanvink@gmail.com>
2018-06-08 14:56:10 +0200

    alghoritm on average prices working, unit_lists corrected and added function call

--- average_prices.py
+++ average_prices.py
@@ -5,21 +5,42 @@ import math
 # this is an example, this loop also runs for the other common commodities
 ###
 
-# import dataset, define dictionary
+# # import dataset, define dictionary
+# ricedict = {}
+# ricedata = pd.read_csv("Riceset.csv")
+# # set first price, and counter
+# meanprice = ricedata['mp_price'][0]
+# counter = 1
+# # loop over dataset, match adjacent lines.
+# for i in range(1,len(ricedata)):
+#     if ricedata['adm_name'][i-1] == ricedata['adm_name'][i]:
+#         # if match, add price to meanprice, increment counter
+#         if ricedata['cm_name'][i-1] == ricedata['cm_name'][i]:
+#             meanprice = meanprice + ricedata['mp_price'][i]
+#             counter += 1
+#         # when no next match, make dictionary entry and reset counter and meanprice
+#         else:
+#             ricedict[str(ricedata['adm_name'][i-1])+'-'+str(ricedata['cm_name'][i-1])] = (meanprice/counter)
+#             meanprice = ricedata['mp_price'][i]
+#             counter = 1
+
 ricedict = {}
-ricedata = pd.read_csv("Riceset.csv")
-# set first price, and counter
+ricedata = pd.read_csv("newRiceDataset.csv", encoding="latin-1")
 meanprice = ricedata['mp_price'][0]
 counter = 1
-# loop over dataset, match adjacent lines.
-for i in range(1,len(ricedata)):
-    if ricedata['adm_name'][i-1] == ricedata['adm_name'][i]:
-        # if match, add price to meanprice, increment counter
-        if ricedata['cm_name'][i-1] == ricedata['cm_name'][i]:
-            meanprice = meanprice + ricedata['mp_price'][i]
-            counter += 1
-        # when no next match, make dictionary entry and reset counter and meanprice
+
+for i in range(0,len(ricedata)):
+    for j in range(i, len(ricedata)):
+        if ricedata['adm0_name'][i] == ricedata['adm0_name'][j]:
+            if ricedata['cm_name'][i] == ricedata['cm_name'][j]:
+                if ricedata['mp_year'][i] == ricedata['mp_year'][j]:
+                    if ricedata['mp_month'][i] == ricedata['mp_month'][j]:
+                        meanprice = meanprice + ricedata['mp_price'][i]
+                        counter += 1
+                        ricedata.drop(ricedata.index[j])
+                        print(ricedata['adm0_name'][i], ricedata.index[j])
         else:
-            ricedict[str(ricedata['adm_name'][i-1])+'-'+str(ricedata['cm_name'][i-1])] = (meanprice/counter)
+            ricedict[str(ricedata['adm0_name'][i])+'-'+str(ricedata['cm_name'][i])+'-'+str(ricedata['mp_month'][i])+'-'+str(ricedata['mp_year'][i])] = (meanprice/counter)
             meanprice = ricedata['mp_price'][i]
             counter = 1
+            break
--- unit_lists.py
+++ unit_lists.py
@@ -1,94 +1,104 @@
 import math
 
 ###
+# this is an example, this loop also runs for the other common commodities
+###
+
+# loop over the dataset and convert prices with convert_prices()
+for i in range(len(ricedata)):
+    ricedata.at[i, 'mp_price'] = convert_prices(ricedata['mp_price'][i], ricedata['um_name'][i])
+    ricedata.at[i, 'um_name'] = 'KG'
+
+
+###
 # a massive function that checks the given measurement trough if-elif statements
-# and converts the weight accordingly
-##
-def convert_weights(weight, measurement)
-    if measurement === 'KG':
-        new_weight = weight
+# and converts the price accordingly
+###
+def convert_prices(price, measurement):
+    if measurement == 'KG':
+        new_price = price
     elif measurement == 'Pound':
-        new_weight = weight*0.45359237
+        new_price = price*0.45359237
     elif measurement == '90 KG':
-        new_weight = weight*90
+        new_price = price*90
     elif measurement == 'MT':
-        new_weight = weight*1000000000
+        new_price = price*1000000000
     elif measurement == '45 KG':
-        new_weight = weight*45
+        new_price = price*45
     elif measurement == '500 KG':
-        new_weight = weight*500
+        new_price = price*500
     elif measurement == 'Marmite':
-        new_weight = weight*2.72155422
+        new_price = price*2.72155422
     elif measurement == '100 KG':
-        new_weight = weight*100
+        new_price = price*100
     elif measurement == '25 KG':
-        new_weight = weight*25
-    #elif measurement == 'Unit':
-    #    new_weight = weight*Onbekend
+        new_price = price*25
+    elif measurement == 'Unit':
+        new_price = price*0.6
     elif measurement == '500 G':
-        new_weight = weight*0.5
+        new_price = price*0.5
     elif measurement == '400 G':
-        new_weight = weight*0.4
+        new_price = price*0.4
     elif measurement == '150 G':
-        new_weight = weight*0.15
-    # elif measurement == 'Loaf':
-    #     new_weight = weight*Onbekend
-elif measurement == '1.5 KG':
-        new_weight = weight*1.5
+        new_price = price*0.15
+    elif measurement == 'Loaf':
+        new_price = price*0.6
+    elif measurement == '1.5 KG':
+        new_price = price*1.5
     elif measurement == '12.5 KG':
-        new_weight = weight*12.5
+        new_price = price*12.5
     elif measurement == '50 KG':
-        new_weight = weight*50
+        new_price = price*50
     elif measurement == 'Pound':
-        new_weight = weight*0.45359237
+        new_price = price*0.45359237
     elif measurement == '750 KG':
-        new_weight = weight*750
+        new_price = price*750
     elif measurement == 'Cuartilla':
-        new_weight = weight*2.875575
+        new_price = price*2.875575
     elif measurement == '125 G':
-        new_weight = weight*0.125
+        new_price = price*0.125
     elif measurement == '11.5 KG':
-        new_weight = weight*11.5
+        new_price = price*11.5
     elif measurement == '380 G':
-        new_weight = weight*0.380
+        new_price = price*0.380
     elif measurement == '385 G':
-        new_weight = weight*0.385
+        new_price = price*0.385
     elif measurement == '185 G':
-        new_weight = weight*0.185
+        new_price = price*0.185
     elif measurement == '85 G':
-        new_weight = weight*0.085
+        new_price = price*0.085
     elif measurement == '91 KG':
-        new_weight = weight*91
+        new_price = price*91
     elif measurement == '650 G':
-        new_weight = weight*0.650
+        new_price = price*0.650
     elif measurement == '115 G':
-        new_weight = weight*0.115
+        new_price = price*0.115
     elif measurement == '350 G':
-        new_weight = weight*0.350
+        new_price = price*0.350
     elif measurement == '1.8 KG':
-        new_weight = weight*1.8
+        new_price = price*1.8
     elif measurement == '2 KG':
-        new_weight = weight*2
+        new_price = price*2
     elif measurement == '300 G':
-        new_weight = weight*0.300
+        new_price = price*0.300
     elif measurement == '160 G':
-        new_weight = weight*0.160
+        new_price = price*0.160
     elif measurement == '5 KG':
-        new_weight = weight*5
+        new_price = price*5
     elif measurement == '200 G':
-        new_weight = weight*0.2
+        new_price = price*0.2
     elif measurement == '10 KG':
-        new_weight = weight*10
+        new_price = price*10
     elif measurement == '750 G':
-        new_weight = weight*0.75
+        new_price = price*0.75
     elif measurement == '12 KG':
-        new_weight = weight*12
+        new_price = price*12
     elif measurement == '18 KG':
-        new_weight = weight*18
+        new_price = price*18
     elif measurement == '60 KG':
-        new_weight = weight*60
+        new_price = price*60
     elif measurement == '3 KG':
-        new_weight = weight*3
+        new_price = price*3
     elif measurement == '3.5 KG':
-        new_weight = weight*3.5
-    return(round(new_weight, 2))
+        new_price = price*3.5
+    return(round(new_price, 2))
