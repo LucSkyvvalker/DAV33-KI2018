72e17365fdd6296341da1dc6d7b4259128fcaea4
Luc Vink <lucjanvink@gmail.com>
2018-06-28 18:17:26 +0200

    .

--- cleaning/sortDatabase.py
+++ cleaning/sortDatabase.py
@@ -1,5 +1,5 @@
 # SORTEER OP DATUM, het aantal colommen in de lege dataframes moeten misschien worden aangepast aangezien "Unnamed: 7"
-# er nu ook tussen staat 
+# er nu ook tussen staat
 def sortbydate(dataframe):
     sortedlist = pd.DataFrame(columns=["adm0_name", "mp_price", "cur_name", "cm_name", "um_name", "mp_month", "mp_year", "Unnamed: 7"])
     for country in dataframe.adm0_name.unique():
@@ -19,35 +19,35 @@ list = sortbydate(rice)
 print(list.to_string())
 
 #Steeds vorige code herhalen om alle datasets te sorteren
-rice = pd.read_csv("finalRiceDataset.csv", encoding='latin-1', low_memory=False, sep=",") 
+rice = pd.read_csv("finalRiceDataset.csv", encoding='latin-1', low_memory=False, sep=",")
 fixedrice = sortbydate(rice)
 fixedrice.to_csv(path_or_buf="sortedFinalFixedRice.csv")
 
-wheat = pd.read_csv("finalWheatDataset.csv", encoding='latin-1', low_memory=False, sep=",") 
+wheat = pd.read_csv("finalWheatDataset.csv", encoding='latin-1', low_memory=False, sep=",")
 fixedwheat = sortbydate(wheat)
 fixedwheat.to_csv(path_or_buf="sortedFinalFixedWheat.csv")
 
-bread = pd.read_csv("finalBreadDataset.csv", encoding='latin-1', low_memory=False, sep=",") 
+bread = pd.read_csv("finalBreadDataset.csv", encoding='latin-1', low_memory=False, sep=",")
 fixedbread = sortbydate(bread)
 fixedbread.to_csv(path_or_buf="sortedFinalFixedBread.csv")
 
-maize = pd.read_csv("finalMaizeDataset.csv", encoding='latin-1', low_memory=False, sep=",") 
+maize = pd.read_csv("finalMaizeDataset.csv", encoding='latin-1', low_memory=False, sep=",")
 fixedmaize = sortbydate(maize)
 fixedmaize.to_csv(path_or_buf="sortedFinalFixedMaize.csv")
 
-millet = pd.read_csv("finalMilletDataset.csv", encoding='latin-1', low_memory=False, sep=",") 
+millet = pd.read_csv("finalMilletDataset.csv", encoding='latin-1', low_memory=False, sep=",")
 fixedmillet = sortbydate(millet)
 fixedmillet.to_csv(path_or_buf="sortedFinalFixedMillet.csv")
 
-sorghum = pd.read_csv("finalSorghumDataset.csv", encoding='latin-1', low_memory=False, sep=",") 
+sorghum = pd.read_csv("finalSorghumDataset.csv", encoding='latin-1', low_memory=False, sep=",")
 fixedsorghum = sortbydate(sorghum)
 fixedsorghum.to_csv(path_or_buf="sortedFinalFixedSorghum.csv")
 
-sugar = pd.read_csv("finalSugarDataset.csv", encoding='latin-1', low_memory=False, sep=",") 
+sugar = pd.read_csv("finalSugarDataset.csv", encoding='latin-1', low_memory=False, sep=",")
 fixedsugar = sortbydate(sugar)
 fixedsugar.to_csv(path_or_buf="sortedFinalFixedSugar.csv")
 
-#maak een mooie staafdiagram 
+#maak een mooie staafdiagram
 '''
 fixedrice = sortbydate(rice)
 
